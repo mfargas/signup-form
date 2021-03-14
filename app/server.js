038d98aad5c50f098d6b6212e7122dad84406152
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-03-13 17:40:56 -0800

    Remove node server and add express.js server

--- app/server.js
+++ app/server.js
@@ -1,21 +1,13 @@
-var http = require('http'); // Import Node.js core module
+const express = require('express');
+const app = express();
+const path = require('path');
 
-var server = http.createServer(function (req, res) {   //create web server
-    if (req.url == '/') { //check the URL of the current request
+app.use(express.static(path.join(__dirname, 'app')));
 
-        // set response header
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-
-        // set response content    
-        res.write('/index.html');
-        res.end();
-
-    }
-    else
-        res.end('Invalid Request!');
-
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
 });
 
-server.listen(5000); //6 - listen for any incoming requests
-
-console.log('Node.js web server at port 5000 is running..')
+app.listen(3333, () => {
+    console.log('Application listening on port 3333!');
+});
